78d6ab70c37dab7ea3e063daaf96dd6ac2b7d24e
Giannis Patouchas <76470267+Taxoupas@users.noreply.github.com>
2022-10-22 17:02:26 +0300

    added smth

--- Cube.java
+++ Cube.java
@@ -2,6 +2,16 @@ public class Cube {
 
     private String[][][] Cube = new String[3][3][6];
 
+    public Cube() {
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<3, j++) {
+                for (int z=0; z<3, z++) {
+                    Cube[i][j][z] = "NoColour";
+                }
+            }
+        }
+    }
+
     public getColour(int x, int y, int z) {
 
         return Cube[x][y][z];
@@ -11,6 +21,10 @@ public class Cube {
 
         Cube[x][y][z] = colour;
 
+    }
+
+    public FillColours() {
+        //to do
     }
 
 }
