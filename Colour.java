860ab5f5155c3c17854fda2037e14d026214fb6d
gogos-pixel <76868701+gogos-pixel@users.noreply.github.com>
2022-10-22 17:18:43 +0300

    new C

---
+++ Colour.java
@@ -0,0 +1,19 @@
+public class Colour{
+    private String  colour;
+    private int Counter;
+
+
+    public void Colour(String colour){
+        this.colour = colour;
+        counter = 0 ;
+    }
+
+    public void SetColour(String colour){
+        this.colour = colour;
+    }
+
+    public String SetColour(String colour){
+        return this.colour;
+    }
+
+}
